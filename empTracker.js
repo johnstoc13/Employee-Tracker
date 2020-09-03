55af812dd65c76465d36304023b504696832c3f9
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-09-03 19:27:32 -0400

    .

---
+++ empTracker.js
@@ -0,0 +1,21 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const consoleTable = require("console.table");
+
+const {Employee, Role, Department} = require("./lib/Creators");
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root1234',
+    database : 'empTracker_DB'
+  });
+
+  connection.connect();
+
+  connection.query('SELECT * from role', function (error, results, fields) {
+    if (error) throw error;
+    console.table(results);
+  });
+
+  connection.end();
