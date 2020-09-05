9588af0e0b2900d750c532177ef6662cb274dbe4
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-09-05 13:06:23 -0400

    getting newEmployee Query

--- empTracker.js
+++ empTracker.js
@@ -3,19 +3,115 @@ const mysql = require("mysql");
 const consoleTable = require("console.table");
 
 const {Employee, Role, Department} = require("./lib/Creators");
+const questions = require("./utils/questions");
 
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : 'root1234',
     database : 'empTracker_DB'
-  });
+});
 
-  connection.connect();
+connection.connect(function(err) {
+  if (err) throw err;
+  startSearch();
+});
 
-  connection.query('SELECT * from employee', function (error, results, fields) {
-    if (error) throw error;
+function startSearch() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees By Department",
+        "View All Employees By Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Exit"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "View All Employees":
+        viewAll();
+        break;
+
+      case "View All Employees By Department":
+        multiSearch();
+        break;
+
+      case "View All Employees By Manager":
+        rangeSearch();
+        break;
+
+      case "Add Employee":
+        addEmployee();
+        break;
+
+      case "Remove Employee":
+        songSearch();
+        break;
+
+      case "Exit":
+        connection.end();
+        break;
+      }
+    });
+}
+
+const viewAll = () => {
+  connection.query('SELECT * from employee', function (err, results) {
+    if (err) throw err;
     console.table(results);
+    startSearch();
   });
+}
 
-  connection.end();
+// const viewByDepartment = () => {
+//   const query = 'SELECT * from employee'
+//   connection.query(query, function (err, results) {
+//     if (err) throw err;
+//     console.table(results);
+//     startSearch();
+//   });
+// }
+
+
+// Prompt user for questions
+const promptQuestions = (type) => {
+  return inquirer.prompt(questions[type]);
+};
+
+
+const addEmployee = () => {
+  promptQuestions("addEmployee").then((res) => {
+
+    let roleNumber;
+    console.log(res.role);
+
+    if (res.role= "Sales Lead") {
+      // console.log("You chose sales lead!");
+      roleNumber = "1";
+    } else if (res.role = "Salesperson") {
+      roleNumber = "2";
+    } else if (res.role = "Lead Engineer") {
+      roleNumber = "3";
+    } else if (res.role = "Software Engineer") {
+      roleNumber = "4";
+    } else if (res.role = "Accountant") {
+      roleNumber = "5";
+    } else if (res.role = "Legal Team Lead") {
+      roleNumber = "6";
+    } else roleNumber = "7";
+
+    // console.log(roleNumber);
+
+    const query1 = `INSERT INTO employee (first_name, last_name, role_id) VALUES ("${res.firstname}", "${res.lastname}", ${res.role})`;
+    const query2 = `INSERT INTO role ()`
+    // console.log(res.role);
+    // console.log(query1);
+    // connection.query1('INSERT name')
+  });
+}
---
+++ utils/questions.js
@@ -0,0 +1,173 @@
+// Empty arrays to check for duplication
+const ids = [];
+const emails = [];
+const offices = [];
+const githubs = [];
+
+// Credit:  https://www.npmjs.com/package/inquirer
+// Credit:  https://stackoverflow.com/questions/35499782/how-to-accept-space-in-regex
+// Validation functions
+// const validateName = (name) => {
+//     const namePattern = /^[a-zA-Z]+(?:\s[a-zA-Z]+)?$/;
+//     if (!name) {
+//         return "Please fill out all fields";
+//     }
+//     else if (!namePattern.test(name)) {
+//         return "Name must be letters only";
+//     }
+//     return true;
+// };
+
+// const validateId = (id) => {
+//     const idPattern = /^[0-9]{3,10}$/;
+//     if (!id) {
+//         return "Please enter an ID";
+//     }
+//     else if (ids.includes(id)) {
+//         return "ID already used, please enter a unique ID";
+//     }
+//     else if (!idPattern.test(id)) {
+//         return "ID must be 3-10 numbers in length";
+//     }
+//     return true;
+// };
+
+// const validateEmail = (email) => {
+//     const emailPattern = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/i;
+//     if (!email) {
+//         return "Please enter an email";
+//     }
+//     else if (emails.includes(email)) {
+//         return "Email already used, please enter a unique Email";
+//     }
+//     else if (!emailPattern.test(email)) {
+//         return "Please enter a valid email address";
+//     }
+//     return true;
+// };
+
+// const validateOffice = (office) => {
+//     const officePattern = /^[0-9]{1,5}$/;
+//     if (!office) {
+//         return "Please enter an office number";
+//     }
+//     else if (!officePattern.test(office)) {
+//         return "Office number must be 1-5 numbers in length";
+//     }
+//     return true;
+// };
+
+// const validateSchool = (school) => {
+//     const schoolPattern = /^[a-zA-Z\s]+$/;
+//     if (!school) {
+//         return "Please enter a school";
+//     }
+//     else if (!schoolPattern.test(school)) {
+//         return "School name must be letters only";
+//     }
+//     return true;
+// };
+
+// const manager = [
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "What is your manager's name?",
+//         validate: validateName
+//     },
+//     {
+//         type: "input",
+//         name: "id",
+//         message: "What is your manager's id?",
+//         validate: validateId
+//     },
+//     {
+//         type: "input",
+//         name: "email",
+//         message: "What is your manager's email?",
+//         validate: validateEmail
+//     },
+//     {
+//         type: "input",
+//         name: "office",
+//         message: "What is your manager's office number?",
+//         validate: validateOffice
+//     },
+// ];
+
+// const intern = [
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "What is your intern's name?",
+//         validate: validateName
+//     },
+//     {
+//         type: "input",
+//         name: "id",
+//         message: "What is your intern's id?",
+//         validate: validateId
+//     },
+//     {
+//         type: "input",
+//         name: "email",
+//         message: "What is your intern's email?",
+//         validate: validateEmail
+//     },
+//     {
+//         type: "input",
+//         name: "school",
+//         message: "What is your intern's school?",
+//         validate: validateSchool
+//     },
+// ];
+
+const addEmployee = [
+    {
+        type: "input",
+        name: "firstname",
+        message: "What is the employee's first name?"
+    },
+    {
+        type: "input",
+        name: "lastname",
+        message: "What is the employee's last name?"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "What is the employee's role?",
+        choices: [
+            "Sales Lead",
+            "Salesperson",
+            "Lead Engineer",
+            "Software Engineer",
+            "Accountant",
+            "Legal Team Lead",
+            "Lawyer"
+        ]
+    },
+    {
+        type: "input",
+        name: "manager",
+        message: "Who is the employee's manager?"
+    },
+];
+
+const startQuestions = [
+    {
+        type: "list",
+        name: "start",
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "View All Employees By Department",
+            "View All Employees By Manager",
+            "Add Employee",
+            "Remove Employee",
+            "Exit"
+        ]
+    },
+];
+
+module.exports = { ids, emails, offices, githubs, manager, intern, addEmployee, startQuestions };
