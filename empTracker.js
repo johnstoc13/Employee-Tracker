e91b2b6536f87561e0b828795f6d6330e7ef0cf2
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-09-06 11:09:37 -0400

    Remove Employee WORKS!!!!

--- empTracker.js
+++ empTracker.js
@@ -2,14 +2,14 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 const consoleTable = require("console.table");
 
-const {Employee, Role, Department} = require("./lib/Creators");
+const { Employee, Role, Department } = require("./lib/Creators");
 const questions = require("./utils/questions");
 
 const connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'root1234',
-    database : 'empTracker_DB'
+  host: 'localhost',
+  user: 'root',
+  password: 'root1234',
+  database: 'empTracker_DB'
 });
 
 connection.connect((err) => {
@@ -34,29 +34,36 @@ function init() {
     })
     .then((answer) => {
       switch (answer.action) {
-      case "View All Employees":
-        viewAll();
-        break;
 
-      case "View All Employees By Department":
-        multiSearch();
-        break;
+        case "View All Employees":
+          // Still need to figure out JOINING all 3 tables...
+          viewAll();
+          break;
 
-      case "View All Employees By Manager":
-        rangeSearch();
-        break;
+        case "View All Employees By Department":
+          // Not Started
+          viewAllByDepartment();
+          break;
 
-      case "Add Employee":
-        addEmployee();
-        break;
+        case "View All Employees By Manager":
+          // Not Started
+          viewAllByManager();
+          break;
 
-      case "Remove Employee":
-        removeEmployee();
-        break;
+        case "Add Employee":
+          // Still need to add employee manager (after ViewAll is complete)
+          addEmployee();
+          break;
 
-      case "Exit":
-        connection.end();
-        break;
+        case "Remove Employee":
+          // COMPLETE!!!
+          removeEmployee();
+          break;
+
+        case "Exit":
+          // COMPLETE!!!
+          connection.end();
+          break;
       }
     });
 }
@@ -70,16 +77,6 @@ const viewAll = () => {
     init();
   });
 }
-
-// const viewByDepartment = () => {
-//   const query = 'SELECT * from employee'
-//   connection.query(query, function (err, results) {
-//     if (err) throw err;
-//     console.table(results);
-//     startSearch();
-//   });
-// }
-
 
 // Prompt user for questions
 const promptQuestions = (type) => {
@@ -105,77 +102,55 @@ const addEmployee = () => {
     } else if (res.role == "Legal Team Lead") {
       roleNumber = "6";
     } else roleNumber = "7";
+    // Query to add employee
     const query = `INSERT INTO employee (first_name, last_name, role_id) VALUES ("${res.firstname}", "${res.lastname}", ${roleNumber})`;
     connection.query(query, function (err) {
       if (err) throw err;
       console.log(`Added ${res.firstname} ${res.lastname} to the database!`);
+      // Start program over from beginning
       init();
     })
   });
 }
 
 const removeEmployee = () => {
+  // Set up empty arrays to use later
   let empArray = [];
   let idArray = [];
+  // Query for IDs and Names in ASC order
   const query = 'SELECT employee.id, concat(first_name, " ", last_name) AS employee FROM employee ORDER BY Employee ASC'
   connection.query(query, (err, res) => {
     if (err) throw err;
-    console.log("#1", res);
-
-    // empArray.push(res);
-    // console.log("HEEEERREE", empArray);
-
-    empArray = res.map(obj => obj.employee);
-    console.log("#2", empArray);
-
-    idArray = res.map(obj => (`${obj.id}, "${obj.employee}"`));
-    console.log("#3", idArray);
+    // console.log("#1", res);
     
+    empArray = res.map(obj => obj.employee);
+    // console.log("#2", empArray);
 
+    idArray = res.map(obj => (`${obj.id}, ${obj.employee}`));
+    // console.log("#3", idArray);
 
+    // Ask user which employee to remove
     inquirer.prompt({
       name: "employee",
       type: "list",
       message: "Which employee would you like to remove?",
       choices: empArray
     }).then((emp) => {
-      console.log("#4", emp);
-      console.log("THIS", emp.employee);
-
-      function getKeyByValue(object, value) {
-        return Object.keys(object).find(key => object[key] === value);
-      }
-      const map = idArray;
-      console.log(getKeyByValue(map, emp.employee));
-      // let chosenId = idArray.find(element => element == emp.employee);
-        
-          // console.log("This is what I found!", id);
-          // return element;
-        
-      
-      // console.log("#6", chosenId);
-      // if (emp.employee == obj.employee)
-    })
-    // console.log("#2", empArray);
-  });
-  // console.log("Made it this far!!!");
-}
-
-// Need a function to call in "choices" that loops through
-// array of all employees (with IDs attached) and then 
-// displays only the employees as choices. Then compare (emp)
-// to that choice and delete using a SQL query WHERE employee ID = ID
-
-
-
-
-
-const viewEmployees = () => {
-  let newArray = [];
-  connection.query('SELECT first_name, last_name from employee', function (err, results) {
-    if (err) throw err;
-    newArray = results.map(obj => {
-      return `${obj.first_name} ${obj.last_name}`;
+      // Then loop through all choices to match user choice
+      idArray.forEach(person => {
+        let choices = person.split(",")[1].trim();
+        if (choices == emp.employee) {
+          let chosenId = person.split(",")[0];
+          // Query to remove the chosen employee
+          const query = `DELETE from employee WHERE id = ${chosenId};`
+          connection.query(query, (err, res) => {
+            if (err) throw err;
+            console.log(`\n ${emp.employee} successfully removed! \n`);
+            // Start program over from beginning
+            init();
+          })
+        }
+      })
     })
   });
 }
