3a3be7ae932d64c699136cb50cff7191c2dbb4bc
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-09-07 15:28:09 -0400

    view, add, update_role, remove all done!!

--- empTracker.js
+++ empTracker.js
@@ -19,24 +19,26 @@ connection.connect((err) => {
 
 // Main function to start program
 function init() {
+
   inquirer.prompt({
-      name: "action",
-      type: "list",
-      message: "What would you like to do?",
-      choices: [
-        "View All Employees",
-        "View All Employees By Department",
-        "View All Employees By Manager",
-        "Add Employee",
-        "Remove Employee",
-        "Exit"
-      ]
-    })
+    name: "action",
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "View All Employees",
+      "View All Employees By Department",
+      "View All Employees By Manager",
+      "Add Employee",
+      "Update Employee Role",
+      "Remove Employee",
+      "Exit"
+    ]
+  })
     .then((answer) => {
       switch (answer.action) {
 
         case "View All Employees":
-          // Still need to figure out JOINING all 3 tables...
+          // COMPLETE!!!
           viewAll();
           break;
 
@@ -51,8 +53,13 @@ function init() {
           break;
 
         case "Add Employee":
-          // Still need to add employee manager (after ViewAll is complete)
+          // COMPLETE!!!
           addEmployee();
+          break;
+
+        case "Update Employee Role":
+          // COMPLETE!!!
+          updateEmployeeRole();
           break;
 
         case "Remove Employee":
@@ -69,9 +76,8 @@ function init() {
 }
 
 // Query to view all employees and data
-// Still need to get manager info  ******************************
 const viewAll = () => {
-  let query = 'SELECT first_name, last_name, title, salary from employee INNER JOIN role ON employee.role_ID = role.id;'
+  let query = 'SELECT e.first_name AS "First Name", e.last_name AS "Last Name", role.title AS Title, role.salary AS Salary, IFNULL((concat(m.first_name, " " ,  m.last_name)), "N/A") AS Manager, department.name AS Department FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN Department ON role.department_id = department.id ORDER BY e.first_name ASC;'
   connection.query(query, function (err, res) {
     if (err) throw err;
     console.log("\n");
@@ -82,6 +88,7 @@ const viewAll = () => {
   });
 }
 
+// ******* NOT USING CURRENTLY *******
 // Prompt user for questions
 const promptQuestions = (type) => {
   return inquirer.prompt(questions[type]);
@@ -89,36 +96,96 @@ const promptQuestions = (type) => {
 
 
 const addEmployee = () => {
-  promptQuestions("addEmployee").then((res) => {
 
-    // Variable to get role_id
-    let roleNumber;
+  let mgrArray = [];
+  let mgrIdArray = [];
 
-    // Run through all responses to set the role_id
-    if (res.role == "Sales Lead") {
-      roleNumber = "1";
-    } else if (res.role == "Salesperson") {
-      roleNumber = "2";
-    } else if (res.role == "Lead Engineer") {
-      roleNumber = "3";
-    } else if (res.role == "Software Engineer") {
-      roleNumber = "4";
-    } else if (res.role == "Accountant") {
-      roleNumber = "5";
-    } else if (res.role == "Legal Team Lead") {
-      roleNumber = "6";
-    } else roleNumber = "7";
+  let query = ('SELECT DISTINCT (concat(m.first_name, " " ,  m.last_name)) AS manager, e.manager_id AS id FROM employee e LEFT JOIN employee m ON e.manager_id = m.id WHERE e.manager_id IS NOT NULL;')
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    mgrArray = res.map(obj => (`${obj.manager}`));
+    mgrIdArray = res.map(obj => (`${obj.id}, ${obj.manager}`));
+    // console.log(mgrArray);
+    // console.log(mgrIdArray);
 
-    // Query to add employee
-    const query = `INSERT INTO employee (first_name, last_name, role_id) VALUES ("${res.firstname}", "${res.lastname}", ${roleNumber})`;
-    connection.query(query, function (err) {
-      if (err) throw err;
-      console.log(`Added ${res.firstname} ${res.lastname} to the database!`);
-      
-      // Start program over
-      init();
-    })
-  });
+    inquirer.prompt(
+      [
+        {
+          type: "input",
+          name: "firstname",
+          message: "What is the employee's first name?"
+        },
+        {
+          type: "input",
+          name: "lastname",
+          message: "What is the employee's last name?"
+        },
+        {
+          type: "list",
+          name: "role",
+          message: "What is the employee's role?",
+          choices: [
+            "Sales Lead",
+            "Salesperson",
+            "Lead Engineer",
+            "Software Engineer",
+            "Accountant",
+            "Legal Team Lead",
+            "Lawyer"
+          ]
+        },
+        {
+          type: "list",
+          name: "manager",
+          message: "Who is the employee's manager?",
+          choices: mgrArray
+        }
+      ]
+    )
+      .then((res) => {
+
+
+        // Variable to get role_id
+        let roleNumber;
+
+        // Run through all responses to set the role_id
+        if (res.role == "Sales Lead") {
+          roleNumber = "1";
+        } else if (res.role == "Salesperson") {
+          roleNumber = "2";
+        } else if (res.role == "Lead Engineer") {
+          roleNumber = "3";
+        } else if (res.role == "Software Engineer") {
+          roleNumber = "4";
+        } else if (res.role == "Accountant") {
+          roleNumber = "5";
+        } else if (res.role == "Legal Team Lead") {
+          roleNumber = "6";
+        } else roleNumber = "7";
+
+
+        // console.log(mgrIdArray);
+        mgrIdArray.forEach(person => {
+          // Credit:  Ask BCS helped me find a way to compare choices by using SPLIT
+          let choices = person.split(",")[1].trim();
+          if (choices == res.manager) {
+            let managerId = person.split(",")[0];
+            // console.log("####", managerId);
+
+
+            // Query to add employee
+            const query = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${res.firstname}", "${res.lastname}", ${roleNumber}, ${managerId})`;
+            connection.query(query, function (err) {
+              if (err) throw err;
+              console.log(`\n Added ${res.firstname} ${res.lastname} to the database! \n`);
+
+              // Start program over
+              init();
+            })
+          }
+        })
+      });
+  })
 }
 
 const removeEmployee = () => {
@@ -153,6 +220,82 @@ const removeEmployee = () => {
           connection.query(query, (err, res) => {
             if (err) throw err;
             console.log(`\n ${emp.employee} successfully removed! \n`);
+
+            // Start program over
+            init();
+          })
+        }
+      })
+    })
+  });
+}
+
+
+const updateEmployeeRole = () => {
+  // Set up empty arrays to use later
+  let empArray = [];
+  let idArray = [];
+
+  // Query for IDs and Names in ASC order
+  const query = 'SELECT employee.id, concat(first_name, " ", last_name) AS employee FROM employee ORDER BY Employee ASC'
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    empArray = res.map(obj => obj.employee);
+    idArray = res.map(obj => (`${obj.id}, ${obj.employee}`));
+
+    // Ask user which employee to remove
+    inquirer.prompt([
+      {
+        name: "employee",
+        type: "list",
+        message: "Which employee would you like to update?",
+        choices: empArray
+      },
+      {
+        name: "role",
+        type: "list",
+        message: "Which new role would you like to use?",
+        choices: [
+          "Sales Lead",
+          "Salesperson",
+          "Lead Engineer",
+          "Software Engineer",
+          "Accountant",
+          "Legal Team Lead",
+          "Lawyer"
+        ]
+      }
+    ]).then((emp) => {
+
+      // Then loop through all choices to match user choice
+      idArray.forEach(person => {
+        let choices = person.split(",")[1].trim();
+        if (choices == emp.employee) {
+          let chosenId = person.split(",")[0];
+
+          // Variable to get role_id
+          let roleNumber;
+
+          // Run through all responses to set the role_id
+          if (emp.role == "Sales Lead") {
+            roleNumber = "1";
+          } else if (emp.role == "Salesperson") {
+            roleNumber = "2";
+          } else if (emp.role == "Lead Engineer") {
+            roleNumber = "3";
+          } else if (emp.role == "Software Engineer") {
+            roleNumber = "4";
+          } else if (emp.role == "Accountant") {
+            roleNumber = "5";
+          } else if (emp.role == "Legal Team Lead") {
+            roleNumber = "6";
+          } else roleNumber = "7";
+
+          // Query to remove the chosen employee
+          const query = `UPDATE employee SET role_id = ${roleNumber} WHERE id = ${chosenId};`
+          connection.query(query, (err, res) => {
+            if (err) throw err;
+            console.log(`\n ${emp.employee}'s role successfully updated to ${emp.role}! \n`);
 
             // Start program over
             init();
