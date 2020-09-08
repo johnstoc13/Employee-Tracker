a31acac0c5c12d201da6e2412d6e995ce825eb1b
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-09-07 21:01:49 -0400

    add new role working!

--- empTracker.js
+++ empTracker.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
-const consoleTable = require("console.table");
+const consoleTable = require("console.table")
 
 const { Employee, Role, Department } = require("./lib/Creators");
 const questions = require("./utils/questions");
@@ -26,9 +26,10 @@ function init() {
     message: "What would you like to do?",
     choices: [
       "View All Employees",
-      "View All Employees By Department",
-      "View All Employees By Manager",
+      "View Departments",
+      "View Manager Database",
       "Add Employee",
+      "Add New Role",
       "Update Employee Role",
       "Remove Employee",
       "Exit"
@@ -42,19 +43,24 @@ function init() {
           viewAll();
           break;
 
-        case "View All Employees By Department":
+        case "View Departments":
           // COMPLETE!!!
-          viewAllByDepartment();
+          viewDepartments();
           break;
 
-        case "View All Employees By Manager":
+        case "View Manager Database":
           // COMPLETE!!!
-          viewAllByManager();
+          viewManagers();
           break;
 
         case "Add Employee":
           // COMPLETE!!!
           addEmployee();
+          break;
+
+        case "Add New Role":
+          // COMPLETE!!!
+          addNewRole();
           break;
 
         case "Update Employee Role":
@@ -77,7 +83,7 @@ function init() {
 
 // Query to view all employees and data
 const viewAll = () => {
-  let query = 'SELECT e.first_name AS "First Name", e.last_name AS "Last Name", role.title AS Title, role.salary AS Salary, IFNULL((concat(m.first_name, " " ,  m.last_name)), "N/A") AS Manager, department.name AS Department FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN Department ON role.department_id = department.id ORDER BY e.first_name ASC;'
+  let query = 'SELECT e.first_name AS "First Name", e.last_name AS "Last Name", role.title AS Title, role.salary AS Salary, IFNULL((concat(m.first_name, " " ,  m.last_name)), "N/A") AS Manager, department.name AS Department FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN Department ON role.department_id = department.id ORDER BY e.first_name ASC;';
   connection.query(query, function (err, res) {
     if (err) throw err;
     console.log("\n");
@@ -86,35 +92,59 @@ const viewAll = () => {
     // Start program over
     init();
   });
-}
+};
 
-// Query to view all employees by department
-const viewAllByDepartment = () => {
-  let query = 'SELECT department.name AS Department, e.first_name AS "First Name", e.last_name AS "Last Name", role.title AS Title, role.salary AS Salary, IFNULL((concat(m.first_name, " " ,  m.last_name)), "N/A") AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN Department ON role.department_id = department.id ORDER BY department.name, e.first_name ASC;'
+// Query to view all departments
+const viewDepartments = () => {
+
+  let query = 'SELECT DISTINCT department.name AS department FROM employee e LEFT JOIN role ON e.role_id = role.id LEFT JOIN Department ON role.department_id = department.id;';
   connection.query(query, function (err, res) {
     if (err) throw err;
-    console.log("\n");
-    console.table(res);
+    deptArray = res.map(obj => (`${obj.department}`));
+    inquirer.prompt({
+      name: "department",
+      type: "list",
+      message: "Which department would you like to view?",
+      choices: deptArray
+    }).then((res) => {
+      let query = `SELECT department.name AS Department, e.first_name AS "First Name", e.last_name AS "Last Name", role.title AS Title, role.salary AS Salary, IFNULL((concat(m.first_name, " " ,  m.last_name)), "N/A") AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id LEFT JOIN role ON e.role_id = role.id LEFT JOIN Department ON role.department_id = department.id WHERE department.name = "${res.department}";`;
+      connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.log("\n");
+        console.table(res);
 
-    // Start program over
-    init();
+        // Start program over
+        init();
+      });
+    });
   });
-}
+};
 
 // Query to view all employees by manager
-const viewAllByManager = () => {
-  let query = 'SELECT IFNULL((concat(m.first_name, " " ,  m.last_name)), "N/A") AS Manager, e.first_name AS "First Name", e.last_name AS "Last Name", role.title AS Title, role.salary AS Salary, department.name AS Department FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN Department ON role.department_id = department.id ORDER BY m.first_name, e.first_name ASC;'
+const viewManagers = () => {
+
+  let query = 'SELECT DISTINCT IFNULL((concat(m.first_name, " " ,  m.last_name)), "N/A") AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id;';
   connection.query(query, function (err, res) {
     if (err) throw err;
-    console.log("\n");
-    console.table(res);
+    managerArray = res.map(obj => (`${obj.manager}`));
+    inquirer.prompt({
+      name: "manager",
+      type: "list",
+      message: "Which manager database would you like to view?",
+      choices: managerArray
+    }).then((res) => {
+      let query = `SELECT IFNULL((concat(m.first_name, " " ,  m.last_name)), "N/A") AS Manager, e.first_name AS "First Name", e.last_name AS "Last Name", role.title AS Title, role.salary AS Salary, department.name AS Department FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN Department ON role.department_id = department.id WHERE (concat(m.first_name, " ", m.last_name)) = "${res.manager}";`;
+      connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.log("\n");
+        console.table(res);
 
-    // Start program over
-    init();
+        // Start program over
+        init();
+      });
+    });
   });
-}
-
-
+};
 
 // ******* NOT USING CURRENTLY *******
 // Prompt user for questions
@@ -122,13 +152,11 @@ const promptQuestions = (type) => {
   return inquirer.prompt(questions[type]);
 };
 
-
 const addEmployee = () => {
 
   let mgrArray = [];
   let mgrIdArray = [];
-
-  let query = ('SELECT DISTINCT (concat(m.first_name, " " ,  m.last_name)) AS manager, e.manager_id AS id FROM employee e LEFT JOIN employee m ON e.manager_id = m.id WHERE e.manager_id IS NOT NULL;')
+  let query = ('SELECT DISTINCT (concat(m.first_name, " " ,  m.last_name)) AS manager, e.manager_id AS id FROM employee e LEFT JOIN employee m ON e.manager_id = m.id WHERE e.manager_id IS NOT NULL;');
   connection.query(query, function (err, res) {
     if (err) throw err;
     mgrArray = res.map(obj => (`${obj.manager}`));
@@ -168,10 +196,8 @@ const addEmployee = () => {
           message: "Who is the employee's manager?",
           choices: mgrArray
         }
-      ]
-    )
+      ])
       .then((res) => {
-
 
         // Variable to get role_id
         let roleNumber;
@@ -191,15 +217,11 @@ const addEmployee = () => {
           roleNumber = "6";
         } else roleNumber = "7";
 
-
-        // console.log(mgrIdArray);
         mgrIdArray.forEach(person => {
           // Credit:  Ask BCS helped me find a way to compare choices by using SPLIT
           let choices = person.split(",")[1].trim();
           if (choices == res.manager) {
             let managerId = person.split(",")[0];
-            // console.log("####", managerId);
-
 
             // Query to add employee
             const query = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${res.firstname}", "${res.lastname}", ${roleNumber}, ${managerId})`;
@@ -209,12 +231,56 @@ const addEmployee = () => {
 
               // Start program over
               init();
-            })
+            });
+          }
+        });
+      });
+  });
+};
+
+const addNewRole = () => {
+
+  let deptArray = [];
+  let deptIdArray = [];
+  let query = 'SELECT DISTINCT department.id, department.name AS department FROM employee e LEFT JOIN role ON e.role_id = role.id LEFT JOIN Department ON role.department_id = department.id;';
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    deptArray = res.map(obj => (`${obj.department}`));
+    deptIdArray = res.map(obj => (`${obj.id}, ${obj.department}`));
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "role",
+        message: "What is the title of the role you want to add?"
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "What is the salary of this role?"
+      },
+      {
+        type: "list",
+        name: "department",
+        message: "Which department does this role belong to?",
+        choices: deptArray
+      }]).then((answer) => {
+        deptIdArray.forEach(dept => {
+          let choice = dept.split(",")[1].trim();
+          if (choice == answer.department) {
+            let deptId = dept.split(",")[0];
+            const newRoleQuery = `INSERT INTO role (title, salary, department_id) VALUES ("${answer.role}", ${answer.salary}, ${deptId});`;
+            connection.query(newRoleQuery, function (err, res) {
+              if (err) throw err;
+              console.log(`\n Added ${answer.role} to the database! \n`);
+
+              // Start program over
+              init();
+            });
           }
         })
       });
-  })
-}
+  });
+};
 
 const removeEmployee = () => {
   // Set up empty arrays to use later
@@ -222,7 +288,7 @@ const removeEmployee = () => {
   let idArray = [];
 
   // Query for IDs and Names in ASC order
-  const query = 'SELECT employee.id, concat(first_name, " ", last_name) AS employee FROM employee ORDER BY Employee ASC'
+  const query = 'SELECT employee.id, concat(first_name, " ", last_name) AS employee FROM employee ORDER BY Employee ASC';
   connection.query(query, (err, res) => {
     if (err) throw err;
     empArray = res.map(obj => obj.employee);
@@ -244,19 +310,19 @@ const removeEmployee = () => {
           let chosenId = person.split(",")[0];
 
           // Query to remove the chosen employee
-          const query = `DELETE from employee WHERE id = ${chosenId};`
+          const query = `DELETE from employee WHERE id = ${chosenId};`;
           connection.query(query, (err, res) => {
             if (err) throw err;
             console.log(`\n ${emp.employee} successfully removed! \n`);
 
             // Start program over
             init();
-          })
+          });
         }
-      })
-    })
+      });
+    });
   });
-}
+};
 
 
 const updateEmployeeRole = () => {
@@ -265,7 +331,7 @@ const updateEmployeeRole = () => {
   let idArray = [];
 
   // Query for IDs and Names in ASC order
-  const query = 'SELECT employee.id, concat(first_name, " ", last_name) AS employee FROM employee ORDER BY Employee ASC'
+  const query = 'SELECT employee.id, concat(first_name, " ", last_name) AS employee FROM employee ORDER BY Employee ASC';
   connection.query(query, (err, res) => {
     if (err) throw err;
     empArray = res.map(obj => obj.employee);
@@ -320,16 +386,16 @@ const updateEmployeeRole = () => {
           } else roleNumber = "7";
 
           // Query to remove the chosen employee
-          const query = `UPDATE employee SET role_id = ${roleNumber} WHERE id = ${chosenId};`
+          const query = `UPDATE employee SET role_id = ${roleNumber} WHERE id = ${chosenId};`;
           connection.query(query, (err, res) => {
             if (err) throw err;
             console.log(`\n ${emp.employee}'s role successfully updated to ${emp.role}! \n`);
 
             // Start program over
             init();
-          })
+          });
         }
-      })
-    })
+      });
+    });
   });
-}
+};
